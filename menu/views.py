1558edfe576f1b5910f3e124ade33853f5482941
Brittney Kistner <bkistner01@gmail.com>
2014-09-23 06:38:17 -0700

    final commits

--- menu/views.py
+++ menu/views.py
@@ -21,11 +21,11 @@ def register(request):
         form = EmailUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            text_content = 'Thank you for signing up for our website, {}'.format(user.username)
-            html_content = '<h2>Thanks {} {} for signing up!</h2> <div>You joined at {}.  I hope you enjoy using our site</div>'.format(user.first_name, user.last_name, user.date_joined)
-            msg = EmailMultiAlternatives("Welcome!", text_content, settings.DEFAULT_FROM_EMAIL, [user.email])
-            msg.attach_alternative(html_content, "text/html")
-            msg.send()
+            # text_content = 'Thank you for signing up for our website, {}'.format(user.username)
+            # html_content = '<h2>Thanks {} {} for signing up!</h2> <div>You joined at {}.  I hope you enjoy using our site</div>'.format(user.first_name, user.last_name, user.date_joined)
+            # msg = EmailMultiAlternatives("Welcome!", text_content, settings.DEFAULT_FROM_EMAIL, [user.email])
+            # msg.attach_alternative(html_content, "text/html")
+            # msg.send()
             new_user = authenticate(username=request.POST['username'],
                                     password=request.POST['password1'])
             login(request, new_user)
