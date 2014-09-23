132abb120e38f0892d7757be71772d020382b367
Brittney Kistner <bkistner01@gmail.com>
2014-09-23 09:01:11 -0700

    final

--- demo1/settings.py
+++ demo1/settings.py
@@ -106,7 +106,7 @@ DEFAULT_FROM_EMAIL = 'brittkistner@gmail.com'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
